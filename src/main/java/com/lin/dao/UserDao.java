d7817bc73ac5cb507903e6025725dce0ba7168bc
taz372436 <821252358@qq.com>
2017-09-05 10:11:19 +0800

    ssm_shiro

---
+++ src/main/java/com/lin/dao/UserDao.java
@@ -0,0 +1,19 @@
+package com.lin.dao;
+
+import com.lin.domain.User;
+
+public interface UserDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+	User findUserByLoginName(String username);
+}
